6e8af72dac08cd3172ea9c9a7e7401bdca40796e
jacketchan <31905180+Jacket-Chan@users.noreply.github.com>
2020-09-10 04:45:38 -1000

    Why
    Made it even more disgusting

--- domainfinder.js
+++ domainfinder.js
@@ -1,7 +1,4 @@
 function generateDomains(search, tldlist) {
-  var results1 = search.filter(word => tldlist.some(word2 => word.endsWith(word2)));
-  console.log(results1);
-  var results2 = results1.flatMap(word3 => tldlist.filter(word4 => word4 == word3.slice(0 - word4.length)).map(word5 => word3.slice(0, 0 - word5.length)+"."+word5));
-  console.log(results2);
-  return results2;
+  // this function takes in an array of names and an array of top level domains (tlds) and returns an array of matches between them.
+  return search.filter(word => tldlist.some(word2 => word.endsWith(word2))).flatMap(word3 => tldlist.filter(word4 => word4 == word3.slice(0 - word4.length)).map(word5 => word3.slice(0, 0 - word5.length)+"."+word5));
 }
