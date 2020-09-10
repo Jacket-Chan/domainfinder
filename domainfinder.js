75d91e26b0f7589587e45a5fbcce5226604dbd9b
jacketchan <31905180+Jacket-Chan@users.noreply.github.com>
2020-09-10 04:41:00 -1000

    Got it working

--- domainfinder.js
+++ domainfinder.js
@@ -1,4 +1,7 @@
 function generateDomains(search, tldlist) {
-  console.log(tldlist);
-  return [1,2,3,4,5];
+  var results1 = search.filter(word => tldlist.some(word2 => word.endsWith(word2)));
+  console.log(results1);
+  var results2 = results1.flatMap(word3 => tldlist.filter(word4 => word4 == word3.slice(0 - word4.length)).map(word5 => word3.slice(0, 0 - word5.length)+"."+word5));
+  console.log(results2);
+  return results2;
 }
